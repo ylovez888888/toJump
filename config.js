573092f66917acf99ed7f9f0f43c50406625d84c
ylovez888888 <178853303@qq.com>
2018-01-06 19:12:18 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ module.exports = {
   port: 4000,
   magicNumber: 2,
   device: 'ios', // 配置目标设备，android为安卓设备，ios为苹果设备
-  url: 'http://169.254.254.39:8100' // ios设备WebDriverAgent地址
+  url: 'http://169.254.217.135:8100' // ios设备WebDriverAgent地址
 }
